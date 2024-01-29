8363e378f9fb458f8a6b65c9096939d4c0019ef6
vinhvox <62163963+vinhvox@users.noreply.github.com>
2024-01-29 17:52:34 +0700

    init project

--- app/src/main/java/com/vapps/basemvvm_kotlin/view/MainActivity.kt
+++ app/src/main/java/com/vapps/basemvvm_kotlin/view/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.vapps.basemvvm_kotlin.view
 
+import android.content.Intent
 import android.view.View
+import androidx.lifecycle.lifecycleScope
 import com.vapps.basemvvm_kotlin.R
 import com.vapps.common.ui.base.BaseActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : BaseActivity(R.layout.activity_main) {
 
     override fun initViews(layoutView: View) {
+        lifecycleScope.launch {
+            delay(1000)
+            startActivity(Intent(this@MainActivity, Class.forName("com.vapps.applock.view.AppLockActivity")))
+        }
     }
 
 }
---
+++ feature/applock/src/main/java/com/vapps/applock/view/AppLockActivity.kt
@@ -0,0 +1,11 @@
+package com.vapps.applock.view
+
+import android.view.View
+import com.vapps.applock.R
+import com.vapps.common.ui.base.BaseActivity
+
+class AppLockActivity : BaseActivity(R.layout.activity_app_lock) {
+    override fun initViews(layoutView: View) {
+
+    }
+}
